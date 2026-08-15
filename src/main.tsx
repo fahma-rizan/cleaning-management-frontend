644a7aa34f16d4168104b59337d6ea1e5a403311
M.D.G.U. Jayalal <udeshikajayalal1016@gmail.com>
2026-08-15 12:20:29 +0530

    Update App import path in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 
+  /// <reference types="vite/client" />
+
   import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
+  import App from "./App";
   import "./index.css";
+  import 'leaflet/dist/leaflet.css';
 
   createRoot(document.getElementById("root")!).render(<App />);
   
